ad6888774f4b19cbe7a680ea30d5ea435bf6c918
Woohyuk Song <harrywsong@naver.com>
2025-07-12 22:55:14 -0400

    logging fix

--- utils/upload_to_drive.py
+++ utils/upload_to_drive.py
@@ -37,7 +37,7 @@ def upload_log_to_drive(file_path):
         upload_timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
         # Compose filename with timestamp
-        drive_filename = original_name
+        drive_filename = f"{name_part}_{upload_timestamp}"
 
         file_metadata = {
             'name': drive_filename,
