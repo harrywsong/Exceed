3adf7fa250a3260de91d216e41dac7f0a8c2ef81
Woohyuk Song <harrywsong@naver.com>
2025-07-18 14:13:16 -0400

    maybe

--- utils/config.py
+++ utils/config.py
@@ -68,6 +68,11 @@ except json.JSONDecodeError as e:
     print(f"Failed to parse REACTION_ROLE_MAP_JSON: {e}")
     REACTION_ROLE_MAP = {}
 
-GSHEET_CREDENTIALS_PATH = os.getenv("GSHEET_CREDENTIALS_PATH")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+GSHEET_CREDENTIALS_PATH = os.path.join(
+    BASE_DIR,
+    os.getenv('GSHEET_CREDENTIALS_PATH', 'exceed-465801-9a237edcd3b1.json')
+)
 MEMBERS_SHEET_NAME = os.getenv("MEMBERS_SHEET_NAME")
 TEST_SHEET_NAME = os.getenv("TEST_SHEET_NAME")
