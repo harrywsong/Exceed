801db5bca46ba9d45ce49e41123f1cd281eef24d
Woohyuk Song <harrywsong@naver.com>
2025-07-22 14:25:52 -0400

    message history media save

--- cogs/interview.py
+++ cogs/interview.py
@@ -541,7 +541,7 @@ class InterviewRequestCog(commands.Cog):
 
         self.logger = logger_module.get_logger(self.__class__.__name__)
         self.logger.info("인터뷰 요청 기능이 초기화되었습니다.")
-        self.logger = get_logger("interview_cog")
+        self.logger = get_logger(self.__class__.__name__)
         self.private_channel_id = INTERVIEW_PRIVATE_CHANNEL_ID
         self.gspread_client = GSpreadClient(config.GSHEET_CREDENTIALS_PATH, self.logger)
 
